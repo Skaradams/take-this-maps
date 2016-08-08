7fb95bbee3a49236efd7a02fc407d5af371ce3a4
Damien Corticchiato <damien.corticchiato@2spark.com>
2016-08-08 17:46:42 +0200

    adding a RECT wouh

--- take-this.js
+++ take-this.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var img = $('#zelda-overworld');
     canvas.setHeight($('#zelda-overworld').height());
     canvas.setWidth($('#zelda-overworld').width());
-    
+
     // console.log($('#zelda-overworld').height());
     var imgInstance = new fabric.Image(img[0], {
       left: 0,
@@ -11,5 +11,26 @@ $(document).ready(function(){
       angle: 0,
       opacity: 1
     });
+
+    var rect = new fabric.Rect({
+        left: 0,
+        top: 0,
+        fill: 'white',
+        stroke: 'white',
+        strokeWidth: 1,
+        opacity: 0.6,
+        width: 250,
+        height: 150,
+        angle: 0
+    })
+
+    imgInstance.lockMovementX = true;
+    imgInstance.lockMovementY = true;
+    imgInstance.lockScalingX = true;
+    imgInstance.lockScalingY = true;
+    imgInstance.lockRotation = true;
+    imgInstance.hasControls = false;
+    imgInstance.hasBorders = false;
     canvas.add(imgInstance);
+    canvas.add(rect);
 });
